afaf95e5250f4c755f21ff097eceb89f1b4cd5f1
emmanuel-haro <emmanuelkai79@gmail.com>
2025-07-18 12:04:29 +0100

    cors3 remove

--- server/server.js
+++ server/server.js
@@ -6,25 +6,8 @@ const connectDB = require("./config/db");
 const app = express();
 connectDB();
 
-const allowedOrigins = [
-  "http://localhost:5173",                 // local dev
-  "https://week7assignment-vox4.onrender.com", // production
-];
 
-app.use(
-    cors(
-        {
-            origin: (origin, cb) => {
-                // Allow Postman/curl whoch send no origi
-                if (!origin || allowedOrigins.includes(origin)) return
-                    cb(null, true);
-                    cb(new Error("Not allowed by CORS"));
-            },
-                credentials: true,
-                methods: "GET,PUT,POST,DELETE",
-                allowedHeaders: "Content-Type,Authorization",
-        }
-    ));
+app.use( cors());
 app.use(express.json());
 
 app.use("/api/auth", require("./routes/authRoutes"));
